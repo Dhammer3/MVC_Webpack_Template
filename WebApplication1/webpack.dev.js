5eceb44e53063faae8daf8d9f7a4f8ba85b5fb73
dhammer3 <usarmy.dh2015@gmail.com>
2020-04-04 20:03:36 -0500

    Added Babel loader in webpack

--- WebApplication1/webpack.dev.js
+++ WebApplication1/webpack.dev.js
@@ -2,12 +2,19 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development', 
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader?sourceMap']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
         ]
     }
